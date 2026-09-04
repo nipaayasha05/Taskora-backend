3fbb525a264c60394f7d3ab62e4a3f778a5e3d2e
nipaayasha05 <nipaayasha05@gmail.com>
2026-09-04 18:03:31 +0600

    chore:setup biome

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,23 @@
 import cookieParser from "cookie-parser";
-import { Application, Request, Response } from "express";
+import type { Application, Request, Response } from "express";
 import express from "express";
 import cors from "cors";
 import config from "./app/config";
 
-const app:Application=express()
+const app: Application = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(cors(
-    {
-        origin:config.app_url,
-        credentials:true,
-    }
-))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: config.app_url,
+		credentials: true,
+	}),
+);
 
+app.get("/", async (req: Request, res: Response) => {
+	res.send(" Welcome to Taskora");
+});
 
-app.get("/", async(req:Request,res:Response)=>{
-    res.send(" Welcome to Taskora")
-})
-
-export default app
+export default app;
